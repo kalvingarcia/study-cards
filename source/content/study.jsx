551d682d701ecdb784a8fe785ffcad1baaa3adc6
Kalvin Garcia <kalvigarcia@gmail.com>
2025-02-26 09:32:33 -0500

    changing button orientation

--- source/content/study.jsx
+++ source/content/study.jsx
@@ -12,7 +12,6 @@ const useStyles = tss.create(({theme}) => ({
         width: "100%",
         height: "100vh",
         display: "flex",
-        flexDirection: "column",
         justifyContent: "space-between",
     },
     stack: {
@@ -21,12 +20,6 @@ const useStyles = tss.create(({theme}) => ({
         display: "flex",
         alignItems: "center",
         justifyContent: "center"
-    },
-    buttons: {
-        width: "100%",
-        display: "flex",
-        justifyContent: "center",
-        gap: "10px"
     }
 }));
 
@@ -47,15 +40,13 @@ export default function Study({}) {
     const {classes} = useStyles({});
     return (
         <div className={classes.study}>
+            <Button appearance="tonal" onClick={() => setCurrent(current - 1 < 0? 0 : current - 1)}><</Button>
             <div className={classes.stack}>
                 {words.map((word, index) => (
                     <FlashCard key={index} {...word} language="english" />
                 ))[current]}
             </div>
-            <div className={classes.buttons}>
-                <Button appearance="tonal" onClick={() => setCurrent(current - 1 < 0? 0 : current - 1)}>Previous</Button>
-                <Button onClick={() => setCurrent(current + 1 >= words.length? words.length - 1 : current + 1)}>Next</Button>
-            </div>
+            <Button onClick={() => setCurrent(current + 1 >= words.length? words.length - 1 : current + 1)}>></Button>
         </div>
     );
 }

c44ef17c355d16d8a5c39bc93f5ba2ace22d0215
Kalvin Garcia <kalvigarcia@gmail.com>
2025-02-27 13:01:56 -0500

    making the menu more usable in mobile mode

--- source/components/menu.jsx
+++ source/components/menu.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {tss} from './common/theme';
 import {useStudy} from './helper/study';
 import Chip from './common/chip';
+import Button from './common/button';
 
-const useStyles = tss.create(({theme}) => ({
+const useStyles = tss.create(({theme, open}) => ({
     menu: {
         flex: "0 0 auto",
         width: "500px",
@@ -13,14 +14,39 @@ const useStyles = tss.create(({theme}) => ({
         flexDirection: "column",
         gap: "20px",
         backgroundColor: theme.neutral.container.hex(),
+        overflow: "hidden",
 
         "@media (max-width: 1100px)": {
-            height: "250px",
-            width: "100%"
+            position: "absolute",
+            inset: 0,
+            height: open? "500px" : 0,
+            width: "100%",
+            transition: "height 300ms ease-in-out"
+        }
+    },
+    header: {
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+
+        "@media (max-width: 1100px)": {
+            position: "relative",
+            top: "-20px",
         }
     },
     title: {
         fontSize: "1.5rem"
+    },
+    button: {
+        minHeight: "48px",
+        maxHeight: "48px",
+        minWidth: "48px",
+        maxWidth: "48px",
+
+        "@media (min-width: 1100px)": {
+            display: "none"
+        }
     },
     list: {
         width: "100%",
@@ -35,10 +61,17 @@ const useStyles = tss.create(({theme}) => ({
 
 export default function Menu({}) {
     const {studyCategories, toggleStudyCategory, allCategories} = useStudy();
-    const {classes} = useStyles({});
+
+    const [open, setOpen] = useState(false);
+    const {classes} = useStyles({open});
     return (
         <div className={classes.menu}>
-            <span className={classes.title}>Categories to Study</span>
+            <div className={classes.header}>
+                <span className={classes.title}>Categories to Study</span>
+                <Button className={classes.button} appearance={open? "filled" : "outlined"} onClick={() => setOpen(!open)}>
+                    <i className='material-symbols-rounded'>{open? "close" : "menu"}</i>
+                </Button>
+            </div>
             <div className={classes.list}>
                 {allCategories.filter(category => category !== "").map((category, index) => (
                     <Chip
